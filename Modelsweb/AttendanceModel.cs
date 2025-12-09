9448b95569918e73bac6e8367d36d18c3755652f
dvanl <dvanl@GREAT-KINTA>
2025-12-09 14:05:38 +0700

    đổi tên model

--- Modelsweb/AttendanceModel.cs
+++ Modelsweb/AttendanceModel.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace webChamcong.Models
+namespace webChamcong.Modelsweb
 {
     internal class AttendanceModel
     {
--- Modelsweb/DashboardModel.cs
+++ Modelsweb/DashboardModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace webChamcong.Models
+namespace webChamcong.Modelsweb
 {
 
     public class WeeklyAttendanceStat
